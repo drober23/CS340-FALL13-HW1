059bce0dfe715b553f0eefb6988bab9ac53c4894
Diana Roberts <di.rob912@gmail.com>
2013-11-03 21:03:50 -0600

    Code with extra buttons and signals/slots added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,9 +14,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+// Button in original code
 void MainWindow::on_pushButton_clicked()
 {
     std::cout << "pushbutton " << std::endl;
+
 }
 
+//First button added
+void MainWindow::on_pushButton_2_clicked()
+{
+    std::cout << "CLICKED " << std::endl;
+
+}
+
+//Second button added
+void MainWindow::on_pushButton_3_clicked()
+{
+    std::cout << "nothing that exciting " << std::endl;
+
+}
+
+//Had to add a Joke! ;)
+void MainWindow::on_pushButton_4_clicked()
+{
+    std::cout << "to get to the UTTER side " << std::endl;
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -37,6 +37,9 @@ public:
 
 private slots:
     void on_pushButton_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
 
 private:
     Ui::MainWindow *ui;
